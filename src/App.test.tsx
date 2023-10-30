317902f2cf8352bc90f5dd8ecd76e66d418f008b
Dominik Musielski <dominik.musielski@widelab.co>
2023-10-30 09:22:44 +0100

    Add tests, Add gh workflow

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,33 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { fireEvent, render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("Task completion can be toggled", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  const taskDescription = "Setup virtual office";
+  const checkbox = screen.getByLabelText(taskDescription);
+
+  expect(checkbox).not.toBeChecked();
+
+  fireEvent.click(checkbox);
+
+  expect(checkbox).toBeChecked();
+
+  fireEvent.click(checkbox);
+
+  expect(checkbox).not.toBeChecked();
+});
+
+test("Adding a new task", () => {
+  render(<App />);
+
+  const addTaskInput = screen.getByLabelText("New Task Description:");
+  const addTaskButton = screen.getByText("Add Task");
+  const addPhaseInput = screen.getByLabelText("New Phase Name:");
+
+  fireEvent.change(addPhaseInput, { target: { value: "Test" } });
+  fireEvent.change(addTaskInput, { target: { value: "New Task Example" } });
+  fireEvent.click(addTaskButton);
+
+  expect(screen.getByText("New Task Example")).toBeInTheDocument();
 });
